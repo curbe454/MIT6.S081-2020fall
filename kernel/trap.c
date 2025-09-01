7969c7c59be3a057a0356edcbfe24351ccd45b3b
curbe454 <ubuntu@localhost.localdomain>
2025-09-01 23:20:18 +0800

    lazy: lazytests

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,25 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13 || r_scause() == 15){
+    // lab lazy, lazy alloc.
+    uint64 va = r_stval();
+    // printf("page fault: %p\n", va);
+    if(va >= p->sz || va < p->trapframe->sp){
+      p->killed = 1;
+    } else {
+      va = PGROUNDDOWN(va);
+      uint64 newpage = (uint64)kalloc();
+      if(newpage == 0){
+        p->killed = 1;
+      } else {
+        memset((void*)newpage, 0, PGSIZE);
+        if(mappages(p->pagetable, va, PGSIZE, newpage, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+          kfree((void*)newpage);
+          p->killed = 1;
+        }
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
